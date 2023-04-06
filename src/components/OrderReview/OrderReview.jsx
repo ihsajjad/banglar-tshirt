3ea927ebde0d68887cc60d5a8832f625816259f6
ihsajjad <sajjadmeal@gmail.com>
2023-04-06 23:47:15 +0600

    first commit

---
+++ src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h3>Your Orders...</h3>
+        </div>
+    );
+};
+
+export default OrderReview;
